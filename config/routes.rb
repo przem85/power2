e095259e3d190e5be346dae12470d356273d0690
przem85 <przemyslaw.spurek@gmail.com>
2019-04-28 08:03:00 +0000

    ja

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  
+  get  '/help',    to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
+  
+  resources :users
+  
+end
